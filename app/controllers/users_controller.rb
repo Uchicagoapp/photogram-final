37d06e76eb55a5fb2fe7df54059445f8ab5efee9
blackbuj4 <jblackb0@chicagobooth.edu>
2021-08-25 21:42:52 +0000

    Show page mostly working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     render({ :template => "users/index.html.erb" })
   end
 
+  def show 
+    #Parameters: {"path_username"=>"alice"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+
+    #if @the_user == nil
+      #redirect_to("/404")
+    #else
+      render({ :template => "users/show.html.erb" })
+    #end
+  end
+
+
 end
